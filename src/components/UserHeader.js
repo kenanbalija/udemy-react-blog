5494bf06f6ffbef313c1d0c87907d6d9fdafd87b
Kenan Balija <kenanbalija.dev@gmail.com>
2019-01-31 07:18:20 +0100

    commit

---
+++ src/components/UserHeader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchUser } from '../actions';
+
+class UserHeader extends React.Component {
+  render() {
+    return (
+      <div>User header</div>
+    );
+  };
+}
+
+export default connect(null, { fetchUser })(UserHeader);
